937842153fdcdae7f3c9d02137cca946d2839d02
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-09-29 21:54:47 +0200

    disable sse for non-x64

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,9 +2,14 @@
 
 mkdir build && cd build
 
+if [ "${target_platform}" == "linux-aarch64" ] || [ "${target_platform}" == "linux-ppc64le" ]; then
+	$USE_SSE="-DFCL_USE_X64_SSE=OFF"
+fi
+
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
 	  -DCMAKE_BUILD_TYPE=Release \
 	  -DCMAKE_INSTALL_LIBDIR=lib \
+	  $USE_SSE \
       $SRC_DIR
 
 make -j${CPU_COUNT}
