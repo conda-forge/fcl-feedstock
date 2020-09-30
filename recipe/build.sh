635b3508d7ce97075a65789a09124afb0e3ad659
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-09-30 08:30:22 +0200

    correct

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,12 +3,15 @@
 mkdir build && cd build
 
 if [ "${target_platform}" == "linux-aarch64" ] || [ "${target_platform}" == "linux-ppc64le" ]; then
-	$USE_SSE="-DFCL_USE_X64_SSE=OFF"
+	USE_SSE="OFF"
+else
+	USE_SSE="ON"
 fi
 
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
 	  -DCMAKE_BUILD_TYPE=Release \
 	  -DCMAKE_INSTALL_LIBDIR=lib \
+	  -DFCL_USE_X64_SSE=$USE_SSE \
 	  $USE_SSE \
       $SRC_DIR
 
