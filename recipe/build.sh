2fe85f364f973d2e4190a839a0bbaf351539a661
Wolf Vollprecht <w.vollprecht@gmail.com>
2021-04-14 11:40:17 +0200

    do not use sse for osx-arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 
 mkdir build && cd build
 
-if [ "${target_platform}" == "linux-aarch64" ] || [ "${target_platform}" == "linux-ppc64le" ]; then
+if [ "${target_platform}" == "osx-arm64" ] || [ "${target_platform}" == "linux-aarch64" ] || [ "${target_platform}" == "linux-ppc64le" ]; then
 	USE_SSE="OFF"
 else
 	USE_SSE="ON"
@@ -12,7 +12,6 @@ cmake ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$PREFIX \
 	  -DCMAKE_BUILD_TYPE=Release \
 	  -DCMAKE_INSTALL_LIBDIR=lib \
 	  -DFCL_USE_X64_SSE=$USE_SSE \
-	  $USE_SSE \
       $SRC_DIR
 
 make -j${CPU_COUNT}
