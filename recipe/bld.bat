98ada51e11354945e0d57134ee37ad3086203391
Silvio Traversaro <silvio@traversaro.it>
2021-11-13 16:58:19 +0100

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,6 @@
 mkdir build && cd build
 
-cmake -G "Ninja"^
+cmake -G "NMake Makefiles"^
     -DCMAKE_BUILD_TYPE=Release^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%^
@@ -9,8 +9,8 @@ cmake -G "Ninja"^
 
 if errorlevel 1 exit 1
 
-ninja
+nmake
 if errorlevel 1 exit 1
 
-ninja install
+nmake install
 if errorlevel 1 exit 1
